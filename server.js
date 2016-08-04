77dd221765f8c14e234575f09a2c4bc458a4ee96
Erik M Jacobs <erikmjacobs@gmail.com>
2016-08-04 04:54:11 -0400

    Changed pagecount response

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.get('/pagecount', function (req, res) {
   }
   if (db) {
     db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
+      res.send('{ Super cool visitrer: ' + count + '}');
     });
   } else {
     res.send('{ pageCount: -1 }');
